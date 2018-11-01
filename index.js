584a50f671897aff3df9e1f715a3779080160741
matej <matkoplan@gmail.com>
2018-11-01 13:14:58 +0100

    basic app works

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-let express = require('express')
+const express = require('express')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-// Initialize the app
-let app = express();
-
-// Setup server port
+mongoose.connect('mongodb://localhost/tenants');
+const db = mongoose.connection;
 const port = process.env.PORT || 8080;
+let app = express();
+let baseUrl = "/v1";
+
+// body parser must be initiated before any request route
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // init each route separately
-let tenantRoutes = require("./routes/tenantRoutes")
-// Initialize the app
-app.use("/api", tenantRoutes);
+app.use(baseUrl+"/tenants", require("./routes/tenantRoutes"));
+
+// Leave here for easy checking if the app is running.
+app.get('/', (req, res) => res.send('<h1> Tenants API running!</h1>'));
 
 
-// Send message for default URL
-app.get('/', (req, res) => res.send('Hello World with Express'));
-
-// Launch app to listen to specified port
 app.listen(port, function () {
-    console.log("Running RestHub on port " + port);
+    console.log("Running server on port " + port);
 });
