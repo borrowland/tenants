0dfbd8c3b995611c5fd30387887cd595dffb8b94
matej <matkoplan@gmail.com>
2018-11-01 11:28:40 +0100

    major architecture is almost complete

---
+++ index.js
@@ -0,0 +1,21 @@
+let express = require('express')
+
+// Initialize the app
+let app = express();
+
+// Setup server port
+const port = process.env.PORT || 8080;
+
+// init each route separately
+let tenantRoutes = require("./routes/tenantRoutes")
+// Initialize the app
+app.use("/api", tenantRoutes);
+
+
+// Send message for default URL
+app.get('/', (req, res) => res.send('Hello World with Express'));
+
+// Launch app to listen to specified port
+app.listen(port, function () {
+    console.log("Running RestHub on port " + port);
+});
