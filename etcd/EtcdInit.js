ee6241a84eb3103f0de8e50906b6ddd4333bed58
matej <matkoplan@gmail.com>
2018-11-06 13:17:19 +0100

    ETCD is set up and works

--- etcd/EtcdInit.js
+++ etcd/EtcdInit.js
@@ -4,6 +4,8 @@ let etcd = new Etcd(etcdUrls);
 
 let watcher2 = etcd.watcher("/tenants/v1/message", null, {recursive: true});
 watcher2.on("change", (val) => {
-    console.log("message value changed: " + val);
+    let prevMessage = val.prevNode.value;
+    let current = val.node.value;
+    console.log("message value changed from: \"" + prevMessage + "\" to: \"" + current+"\"");
     // process.env.DB_URI = val;
 });
