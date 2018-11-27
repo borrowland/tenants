f6b35c4db2819338d709881e5520dc2ccd61b69a
MatejKoplan <matkoplan@gmail.com>
2018-11-27 19:21:47 +0100

    updated dockerfile and exposed JSON for grading

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:8
+FROM node:8-alpine
 RUN mkdir /tenants
 WORKDIR /tenants
 COPY . /tenants
--- index.js
+++ index.js
@@ -36,7 +36,17 @@ app.get('/', (req, res) => res.send('<h1> Tenants API running!</h1>'));
 // Check configuration
 app.get('/etcd', (req, res) => res.send(JSON.stringify(config)));
 
-
+app.get('/v1/demo/info', (req, res) => {
+    let a = {
+        "clani": ["jg1278", "mk7509"],
+        "opis_projekta": "Nas projekt implementira aplikacijo za izposojo opreme.",
+        "mikrostoritve": ["http://159.122.186.19:35000/tenants/v1", "http://159.122.186.19:32385/equipment/v1"],
+        "github": ["https://github.com/borrowland/tenants", "https://github.com/borrowland/equipment"],
+        "travis": ["https://travis-ci.org/borrowland/tenants", "https://travis-ci.org/borrowland/equipment"],
+        "dockerhub": ["https://hub.docker.com/r/mkoplan/tenants/", "https://hub.docker.com/r/mkoplan/equipment/"]
+    }
+    res.send(a)
+})
 
 app.listen(port, function () {
     console.log(`Running server on port ${port}. Version: ${version}  Environment: ${environment}`);
